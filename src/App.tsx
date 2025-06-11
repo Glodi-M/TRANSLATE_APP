2ae7ed1ac000d99197c19b4727ec61b6d865c6aa
Glodi Mietete <glodimietete@gmail.com>
2025-06-11 11:34:10 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+        <div>
+          <h1>Welcome to the React App</h1>
+          <p>This is a simple React application.</p>
+        </div>
+    </>
+  )
+}
+
+export default App
