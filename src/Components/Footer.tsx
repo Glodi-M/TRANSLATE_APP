d51082c902c900d0534435b261a905c5019f32d2
Glodi Mietete <glodimietete@gmail.com>
2025-06-23 13:39:14 +0200

    Mise à jour du Footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
         <p>© 2025 Application de Traduction Français en Espagne</p>
-        <p>Conçu par <span>Glodi Mietete</span>,dans le cadre de Summer Camp HETIC</p>
+        <p>Conçu par <span>Glodi Mietete</span>, dans le cadre de Summer Camp HETIC</p>
       </footer>
   )
 }
